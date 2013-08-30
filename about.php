bbee89e9a77db4b43f3dddb020a3674d0d8a758d
Ankush Jain <anku.94@gmail.com>
2013-08-30 19:43:43 +0530

    First Commit

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php include_once('header.php'); ?>
+			<div class="post">
+				<h2 class="title"><a href="#">Welcome to Eklavya</a></h2>
+				<!--p class="meta">
+						<span class="date">September 12, 2012</span>
+						<span class="posted">Posted by <a href="#">Someone</a></span>
+				</p-->
+				<div style="clear: both;">&nbsp;</div>
+                                <div class="entry">
+                                  <p>
+                                      Eklavya Enterprises is an enterprise dealing with computer peripherals in Dombivalli, Maharashtra. We guarantee best prices and prioritise customer satisfaction.
+
+                                  </p>
+				</div>
+			</div>
+<?php include_once('footer.php'); ?>
---
+++ login.php
@@ -0,0 +1,18 @@
+<?php include_once('header.php'); ?>
+			<div class="post">
+				<h2 class="title"><a href="#">Login</a></h2>
+				<!--p class="meta">
+						<span class="date">September 12, 2012</span>
+						<span class="posted">Posted by <a href="#">Someone</a></span>
+				</p-->
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+                                        <table id="login">
+                                        <tr><td>Username</td><td><input type="text"></td></tr>
+                                        <tr><td>Password</td><td><input type="text"></td></tr>
+					
+					</table>
+				        <input type="submit" value="Submit">		
+				</div>
+			</div>
+<?php include_once('footer.php'); ?>
